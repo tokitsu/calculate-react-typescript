5acf32be9b0f574e752d6e9aa30bd7dbc91cf8be
tokitsu <48195108+tokitsu@users.noreply.github.com>
2022-05-25 22:25:05 +0700

    add State type

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,9 +1,18 @@
 import { ButtonPanel } from "./ButtonPanel";
 import { Display } from "./Display";
+import { calculate } from "../logic/calculate";
+import { useState } from "react";
 
 export const Calculator = () => {
+  const [state, setState] = useState<State>({
+    current: "0",
+    operand: 0,
+    operator: null,
+    isNextClear: false
+  });
   const buttonHandler = (code: string) => {
-    console.log(code);
+    const nextState = calculate(code, state);
+    setState(nextState);
   };
   return (
     <div>
--- src/logic/calculate.ts
+++ src/logic/calculate.ts
@@ -1,3 +1,10 @@
-export const calculate = () => {
-  return 0;
+export const calculate = (button: string, state: State): State => {
+  return state;
+};
+
+export type State = {
+  current: string;
+  operand: number;
+  operator: string | null;
+  isNextClear: boolean;
 };
